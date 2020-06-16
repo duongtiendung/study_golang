b68f902430b95aab6b2337082fca37c68d3518b9
dungdt <dungdt@lifetimetech.vn>
2020-06-16 18:08:36 +0700

    commit code sua thu lam cho dung lai khi build de co the nhin ket qua

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
 )
 
 func main() {
@@ -12,29 +9,35 @@ func main() {
 
 	//If statement
 	a := 1
-	fmt.Printf("hãy nhập a /n")
-	fmt.Scanf("%f", &a)
+	fmt.Printf("hãy nhập a \n")
+	fmt.Scanf("%d", &a)
+
+	fmt.Println(a)
 
 	if a > 1 {
-		fmt.Printf("Hiiiii... true")
+		fmt.Printf(" a>1 \n")
 	} else {
-		fmt.Printf("Hiiiii... fales")
+		fmt.Printf(" a=<1 \n")
 	}
 
+	b := 1
+	fmt.Printf("hãy nhập b \n")
+	fmt.Scanf("%d", &b)
+	//For statement
 	sum := 0
 	for i := 0; i < 10000; i++ {
 		sum += i
 	}
 	fmt.Println(sum)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// })
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+	// http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hi")
+	// })
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	// log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
