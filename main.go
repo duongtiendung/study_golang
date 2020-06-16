12ee48d3530f33901df61a242e041a2b9b1c9395
dungdt <dungdt@lifetimetech.vn>
2020-06-16 10:53:57 +0700

    if statement

--- main.go
+++ main.go
@@ -8,7 +8,25 @@ import (
 )
 
 func main() {
-	fmt.Println("web stat")
+	fmt.Println("go stat")
+
+	//If statement
+	a := 1
+	fmt.Printf("hãy nhập a /n")
+	fmt.Scanf("%f", &a)
+
+	if a > 1 {
+		fmt.Printf("Hiiiii... true")
+	} else {
+		fmt.Printf("Hiiiii... fales")
+	}
+
+	sum := 0
+	for i := 0; i < 10000; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
